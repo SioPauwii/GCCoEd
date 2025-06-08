adb560b9d76e6bcc1c27f29c2b4e9ab7170acdfa
Pau <paulocordova3@gmail.com>
2025-06-08 11:52:57 +0800

    official hosting attempt

--- config/database.php
+++ config/database.php
@@ -101,7 +101,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('PGSQL_SSL_MODE', 'prefer'),
         ],
 
         'sqlsrv' => [
